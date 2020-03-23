b3a2673f98a3dfb916e3aa9588d61222e555db2c
Gekif <dzulfikar.maulana@gmail.com>
2020-03-23 10:13:48 +0700

    14. Creating The Views: 3. Creating The Home Page, Part 2, 4. Creating The Home Page, Part 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
 
+    <div class="container">
+        <table class="table">
+              @foreach ($messages as $message)
+                  <tr>
+                      <td>{{ $username }}</td>
+                      <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
+                      <td>{{ $message->text }}</td>
+                      <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
+                      <td>{{ $message->time }}</td>
+                  </tr>
+              @endforeach
+        </table>
+    </div>
+
+
     <form id="chat_form" action="{{ url('insert') }}" method="POST">
         @csrf
         <input type="text" id="chat_dialogue" name="text">
