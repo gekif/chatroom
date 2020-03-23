595673337e9ccb45eea523ed1aa849455ed2ddbe
Gekif <dzulfikar.maulana@gmail.com>
2020-03-23 10:09:42 +0700

    14. Creating The Views: 2. Creating The Home Page, Part 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <form id="chat_form" action="{{ url('insert') }}" method="POST">
+        @csrf
+        <input type="text" id="chat_dialogue" name="text">
+        <input type="submit" class=" btn btn-primary" value="submit">
+    </form>
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
