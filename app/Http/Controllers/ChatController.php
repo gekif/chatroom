2865245cb4e3c4beb5736b68d3dbd3789f53df94
Gekif <dzulfikar.maulana@gmail.com>
2020-03-23 09:20:58 +0700

    11. Creating The Controller: 1. Creating The Insert Function, Part 1

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
     public function insert(Request $request)
     {
-
+        Message::create([
+            'user_id' => Auth::id(),
+            'text' => $request->input('text'),
+            'time' => date('h:i:s:a')
+        ]);
     }
 
 
